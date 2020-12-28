579d720e3a0529e70f442633d58493596d149ba9
sharpevo <sharpevo@outlook.com>
2020-12-28 16:44:18 +0800

    Fix failure bug of archiving

--- cmd/watch/app/app.go
+++ cmd/watch/app/app.go
@@ -159,6 +159,7 @@ func (w *WatchCommand) update(eventName string, chipId string) (string, error) {
 		archivedPath, err := w.archive(chipId)
 		if err != nil {
 			logrus.Errorf("failed to archive %s: %v", chipId, err)
+			return message, nil
 		}
 		logrus.Infof("archived %s: %s", chipId, archivedPath)
 		message = fmt.Sprintf("%s\n- Archive: %s",
