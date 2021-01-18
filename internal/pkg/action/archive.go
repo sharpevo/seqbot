9c64593c4fb367ca192625b6219d0108a4aca22c
sharpevo <sharpevo@outlook.com>
2021-01-18 16:09:41 +0800

    Add stateless archive action

---
+++ internal/pkg/action/archive.go
@@ -0,0 +1,48 @@
+package action
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/sharpevo/seqbot/pkg/util"
+)
+
+const (
+	LAYOUT_ARCHIVE      = "200601"
+	NAME_ARCHIVE_ACTION = "Archive"
+)
+
+type ArchiveAction struct{}
+
+func (a *ArchiveAction) Run(
+	wfqLogPath string,
+	chipId string,
+) (string, error) {
+	rootPath := util.ResultRootPathFromWFQLogPath(wfqLogPath)
+	archivePath, err := getArchivePath(rootPath, time.Now())
+	if err != nil {
+		return "", err
+	}
+	return getOutput(archivePath), os.Rename(
+		filepath.Join(rootPath, chipId),
+		filepath.Join(archivePath, chipId),
+	)
+}
+
+func getArchivePath(rootPath string, timestamp time.Time) (string, error) {
+	archivePath := filepath.Join(rootPath, getArchiveName(timestamp))
+	return archivePath, os.MkdirAll(archivePath, 0755)
+}
+
+func getArchiveName(timestamp time.Time) string {
+	return timestamp.Format(LAYOUT_ARCHIVE)
+}
+
+func getOutput(archivePath string) string {
+	return filepath.Base(archivePath)
+}
+
+func (a *ArchiveAction) Name() string {
+	return NAME_ARCHIVE_ACTION
+}
