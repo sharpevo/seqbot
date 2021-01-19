bc0f6e9cf3cdec1ac39f5fce57543a70b3769762
sharpevo <sharpevo@outlook.com>
2021-01-19 14:12:24 +0800

    Add message

---
+++ cmd/DNBSEQ-T7/app/message.go
@@ -0,0 +1,17 @@
+package app
+
+import "fmt"
+
+type Message struct {
+	sep string
+	msg string
+}
+
+func (m *Message) Add(msg string) {
+	m.msg = fmt.Sprintf("%s%s%s", m.msg, m.sep, msg)
+	fmt.Println(">>", m.msg)
+}
+
+func (m *Message) String() string {
+	return m.msg
+}
