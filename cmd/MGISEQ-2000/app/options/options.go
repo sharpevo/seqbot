cb9b3c24543b6716dadce9101aa29e0b43b3bf96
sharpevo <sharpevo@outlook.com>
2021-01-28 16:20:32 +0800

    Add Mgiseq2000Options

--- cmd/MGISEQ-2000/app/options/options.go
+++ cmd/MGISEQ-2000/app/options/options.go
@@ -2,56 +2,19 @@ package options
 
 import (
 	"flag"
-	"fmt"
-	"os"
-	"strings"
 )
 
-type arrayFlag []string
-
-func (a *arrayFlag) String() string {
-	return strings.Join(*a, ",")
+type Mgiseq2000Options struct {
+	DataPath string
 }
 
-func (a *arrayFlag) Set(value string) error {
-	*a = append(*a, value)
-	return nil
-}
-
-type Options struct {
-	DataPath   string
-	DingTokens arrayFlag
-	Debug      bool
-}
-
-func AttachOptions(cmd *flag.FlagSet) *Options {
-	options := &Options{}
+func AttachMgiseq2000Options(cmd *flag.FlagSet) *Mgiseq2000Options {
+	options := &Mgiseq2000Options{}
 	cmd.StringVar(
 		&options.DataPath,
 		"data",
 		"",
 		"data path to watch",
 	)
-	cmd.BoolVar(
-		&options.Debug,
-		"debug",
-		false,
-		"show debug message",
-	)
-	cmd.Var(
-		&options.DingTokens,
-		"dingtoken",
-		"token of DingTalk robots",
-	)
-	flag.Usage = func() {
-		fmt.Fprintf(
-			flag.CommandLine.Output(),
-			"Usage of %s:\n  %s %s\n",
-			os.Args[0],
-			os.Args[0],
-			"-data=/path/to/data -dingtoken=token1 -dingtolken=token2",
-		)
-		flag.PrintDefaults()
-	}
 	return options
 }
