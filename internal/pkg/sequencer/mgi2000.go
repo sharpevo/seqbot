193ab0508ea10f48b3d93cc79505a7828a71c3c9
sharpevo <sharpevo@outlook.com>
2021-01-28 16:23:59 +0800

    Add Mgiseq2000 sequencer

---
+++ internal/pkg/sequencer/mgi2000.go
@@ -0,0 +1,74 @@
+package sequencer
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/sharpevo/seqbot/pkg/util"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	BIOINFO_NAME = "BioInfo.csv"
+	PREFIX_DNBID = "DNB ID,"
+
+	TMPL_SUCCESS = "^%s.*_Success.txt$"
+)
+
+var lanes = []string{"L01", "L02", "L03", "L04"}
+
+type Mgiseq2000 struct{}
+
+func (m *Mgiseq2000) GetBarcode(successPath string) (string, error) {
+	dir := filepath.Dir(successPath)
+	idMap := map[string]struct{}{}
+	ids := []string{}
+	for _, l := range lanes {
+		bioinfoPath := filepath.Join(dir, l, BIOINFO_NAME)
+		bioinfoFile, err := os.Open(bioinfoPath)
+		if err != nil {
+			logrus.Warnf("cannot parse dnb id from %s: %v", bioinfoPath, err)
+			continue
+		}
+		defer bioinfoFile.Close()
+		scanner := bufio.NewScanner(bioinfoFile)
+		for scanner.Scan() {
+			splits := strings.Split(scanner.Text(), PREFIX_DNBID)
+			if len(splits) == 2 {
+				if _, ok := idMap[splits[1]]; !ok {
+					ids = append(ids, splits[1])
+					idMap[splits[1]] = struct{}{}
+				}
+				break
+			}
+		}
+	}
+	return strings.Join(ids, ","), nil
+}
+
+func (m *Mgiseq2000) GetSlide(successPath string) (string, error) {
+	return filepath.Base(filepath.Dir(successPath)), nil
+}
+
+func (m *Mgiseq2000) GetArchiveDir(successPath string) (string, error) {
+	return filepath.Join(
+		filepath.Dir(filepath.Dir(successPath)),
+		util.GetArchiveName(time.Now()),
+	), nil
+}
+
+func (m *Mgiseq2000) GetResultDir(successPath string) (string, error) {
+	return filepath.Dir(successPath), nil
+}
+
+func (m *Mgiseq2000) IsSuccess(filePath string) (bool, error) {
+	lastDir := filepath.Base(filepath.Dir(filePath))
+	r := regexp.MustCompile(fmt.Sprintf(TMPL_SUCCESS, lastDir))
+	return r.MatchString(filepath.Base(filePath)), nil
+}
