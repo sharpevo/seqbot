5d652b358fcd6aa6dbee80afdde8ba016c51f676
sharpevo <sharpevo@outlook.com>
2021-01-28 16:21:24 +0800

    Change Run signature

--- internal/pkg/action/action.go
+++ internal/pkg/action/action.go
@@ -1,6 +1,6 @@
 package action
 
 type ActionInterface interface {
-	Run(eventname string, wfqLogPath string, chipId string) (string, error)
+	Run(eventname string, command CommandInterface) (string, error)
 	Name() string
 }
