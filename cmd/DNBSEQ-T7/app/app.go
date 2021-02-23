4cc4791dddd5e50babb0265c80ab0254499753dc
sharpevo <sharpevo@outlook.com>
2021-02-23 15:02:07 +0800

    Update DNBSEQ-T7 sendCommand

--- cmd/DNBSEQ-T7/app/app.go
+++ cmd/DNBSEQ-T7/app/app.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/sharpevo/seqbot/pkg/util"
+	"github.com/sharpevo/seqbot/cmd/send/app"
 )
 
 const (
@@ -67,7 +67,7 @@ func (t *T7Command) Execute() error {
 		}
 		return runCommand.Execute()
 	case CMD_SEND:
-		sendCommand := util.NewSendCommand(sendFlagSet)
+		sendCommand := app.NewSendCommand(sendFlagSet)
 		if err := sendFlagSet.Parse(os.Args[2:]); err != nil {
 			sendFlagSet.PrintDefaults()
 			return err
