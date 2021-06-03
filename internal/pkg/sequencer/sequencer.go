3d45dd2a888de7d14dcce2b57f49b63fe2f616e1
sharpevo <sharpevo@outlook.com>
2021-06-03 15:55:21 +0800

    Add GetExtraExperimentInfo in interface

--- internal/pkg/sequencer/sequencer.go
+++ internal/pkg/sequencer/sequencer.go
@@ -3,6 +3,7 @@ package sequencer
 type SequencerInterface interface {
 	GetSlide(string) (string, error)
 	GetBarcode(string) (string, error)
+	GetExtraExperimentInfo(string) (string, error)
 	GetArchiveDir(string) (string, error)
 	GetResultDir(string) (string, error)
 	GetWfqTime(string) (string, error)
